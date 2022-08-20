7fa78ebd56091086019705497293e45c940a087e
Buseyna <b30mgls@gmail.com>
2022-08-20 22:33:09 +0300

    first commit

--- Performing_Queries/routes/index.js
+++ Performing_Queries/routes/index.js
@@ -1,33 +1,30 @@
 var express = require('express');
 var router = express.Router();
-var mysql = require('mysql');
+const mysql = require('mysql');
 
 var connection = mysql.createConnection({
-  host : 'localhost',
+  host: 'localhost',
   user: 'root',
   password: 'Password1!',
   database: 'sakila'
 });
 
-connection.content(
-  function(err){
-  if(err){
+connection.connect(function(err) {
+  if (err) {
     console.error(err.message);
     return;
   }
-  console.log('Yay, You are connected to the database');
-});
-
-const query = `SELECT * FROM actor LIMIT 10`;
+  console.log('Yay! You are connected to the database!');
+})
+const query = `SELECT * from actor LIMIT 10`;
 
 connection.query(query, (err, results) => {
   if (err) throw err;
- console.log(results);
-});
+  console.log(results);
+})
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  
   res.render('index', { title: 'Express' });
 });
 
